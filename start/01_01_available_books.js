8aa1a4c7bf0349f02980ac5f3f5fdbfe5f0b3c3c
tbinte <tbinte@users.noreply.github.com>
2024-10-29 16:01:44 +0000

    better solution

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,1 +1,41 @@
-// Write your code here
+class Book {
+  constructor(title, author, isbn, numCopies) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+    this.numCopies = numCopies;
+  }
+
+  get availability() {
+    return this.getAvailability();
+  }
+
+  getAvailability() {
+    if (this.numCopies === 0) {
+      return "Out of stock"
+    } else if (this.numCopies < 10) {
+      return "Low stock"
+    } else {
+      return "In Stock"
+    }
+  }
+  
+  sell(numSold = 1) {
+      if (this.numCopies >= numSold) {
+        this.numCopies -=  numSold;
+      } else {
+        this.numCopies = 0;
+      }
+  }
+  
+  restock(numCopies = 5) {
+      this.numCopies += numCopies;
+  }
+}
+
+const hungerGames = new Book("HungerGames", "Ich", "1223234234", 5);
+console.log(hungerGames);
+hungerGames.sell(10);
+console.log(hungerGames.availability);
+console.log(hungerGames);
+
